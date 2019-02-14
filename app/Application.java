4ce83b54af82c71e77759c867c9655f1c9748631
BlackLight <eldin.anthony@gmail.com>
2019-02-14 11:09:56 +0100

    Modification du main

--- app/Application.java
+++ app/Application.java
@@ -13,15 +13,19 @@ public class Application
 	public static void main(String[] args) 
 	{
 	
+			Fournisseur fournisseur = new Fournisseur ("Cheval 3000","Guillaume", "0606060606");
 			Box box = new Box(10,"Fond");
+			Produit produit = new Produit("Karim", 9, fournisseur);
 			Cheval cheval = new Cheval("Ponpon","PurSang","Etalon",box);
 			Cours cours = new Cours (cheval,LocalDate.now());
 			Infrastructure infra = new Infrastructure("Quai 9-3/4","Carrière",true,cours);
+			
 			
 			System.out.println(box);
 			System.out.println(cheval);
 			System.out.println(cours);
 			System.out.println(infra);
+			
 			
 	}
 	
