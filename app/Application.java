ef91f727d958318862f897faf1b4e40eb6d7e16f
BlackLight <eldin.anthony@gmail.com>
2019-02-14 11:59:57 +0100

    Modification du main avec les nouvelles features

--- app/Application.java
+++ app/Application.java
@@ -15,20 +15,40 @@ public class Application
 	public static void main(String[] args) 
 	{
 	
-			Fournisseur fournisseur = new Fournisseur ("Cheval 3000","Guillaume", "0606060606");
+			Fournisseur f1 = new Fournisseur ("Cheval 3000","Guillaume", "0606060606");
+			Fournisseur f2 = new Fournisseur ("New Generation Dada", "Karim", "0707070707");
+			
+			
+			Produit produit1 = new Produit("Hipposandale", 1,f1);
+			Produit produit2 = new Produit("Tapis",9,f1);
+			Produit produit3 = new Produit("Selle",3,f2);
+			
+			f1.ajouterProduit(produit1);
+			f1.ajouterProduit(produit2);
+			System.out.println("Voyons le fournisseur 1 :");
+			System.out.println(f1);
+			f2.ajouterProduit(produit3);
+			System.out.println("----------------------------------------------------------------");
+			System.out.println("Voyons le fournisseur 2 :");
+			System.out.println(f2);
+			f2.supprimerProduit(produit3);
+			
 			Box box = new Box(10,"Fond");
-			Produit produit = new Produit("Hipposandale", 1,fournisseur);
-			Cheval cheval = new Cheval("Ponpon","PurSang","Etalon",box, produit);
+			
+			Cheval cheval = new Cheval("Ponpon","PurSang","Etalon",box, produit1);
+			cheval.ajouterProduit(produit3);
+			System.out.println("----------------------------------------------------------------");
+			System.out.println("Voyons le cheval");
+			System.out.println(cheval);
+			cheval.supprimerProduit(produit1);
+			System.out.println("----------------------------------------------------------------");
+			System.out.println("Voyons le cheval");
+			System.out.println(cheval);
 			
 			Cours cours = new Cours (cheval,LocalDate.now());
 			Infrastructure infra = new Infrastructure("Quai 9-3/4","Carrière",true,cours);
 			
-			System.out.println(fournisseur);
-			System.out.println(box);
-			System.out.println(produit);
-			System.out.println(cheval);
-			System.out.println(cours);
-			System.out.println(infra);
+			
 			
 			
 	}
