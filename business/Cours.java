fa512ba9cf785120241dd6bf733110f0d1a8b628
BlackLight <eldin.anthony@gmail.com>
2019-02-14 11:09:29 +0100

    Mise en place des getters et setters de la classe Cours.java

--- business/Cours.java
+++ business/Cours.java
@@ -5,17 +5,39 @@ import java.time.LocalDate;
  * 
  * 
  * @author Eldin Anthony
- *
+ * Date : 14/02/2019
  */
 public class Cours 
 {
 	private Cheval cheval;
-	private LocalDate date;
+	private LocalDate dateCours;
 
 	public Cours (Cheval cheval, LocalDate date)
 	{
+		setCheval(cheval);
+		setDate(date);
+	}
+	
+	private void setCheval (Cheval cheval)
+	{
 		this.cheval=cheval;
-		this.date=date;
+	}
+	
+	
+	//Pour un cours nous ne renverrons que le nom du cheval 
+	public String getCheval()
+	{
+		return cheval.getNom();
+	}
+	
+	private void setDate(LocalDate date)
+	{
+		this.dateCours=date;
+	}
+	
+	public String getDateCours()
+	{
+		return dateCours.toString();
 	}
 	
 	private String getJour(LocalDate date)
@@ -25,41 +47,37 @@ public class Cours
 		jour = date.getDayOfWeek().toString();
 		switch (jour)
 		{
-		case "MONDAY" : jour = "Lundi";
-						break;
+		case "MONDAY" : 	jour = "Lundi";
+							break;
 						
-		case "TUESDAY" : jour = "Mardi";
-		break;
+		case "TUESDAY" : 	jour = "Mardi";
+							break;
 		
-		case "WEDNESDAY" : jour = "Mercredi";
-		break;
+		case "WEDNESDAY" : 	jour = "Mercredi";
+							break;
 		
-		case "THURSDAY" : jour = "Jeudi";
-		break;
+		case "THURSDAY" : 	jour = "Jeudi";
+							break;
 		
-		case "FRIDAY" : jour = "Jeudi";
-		break;
+		case "FRIDAY" : 	jour = "Jeudi";
+							break;
 		
-		case "SATURDAY" : jour = "Samedi";
-		break;
+		case "SATURDAY" : 	jour = "Samedi";
+							break;
+			
+		case "SUNDAY" : 	jour = "Dimanche";
+							break;
 		
-		case "SUNDAY" : jour = "Dimanche";
-		break;
-		
-		default : jour = "Inconnu";
-		break;
-		
-		}
-		
-		
+		default : 			jour = "Inconnu";
+							break;
+		}	
 		return jour;
-		
 	}
 	
 	@Override
 	public String toString()
 	{
-		return "Le cours avec le cheval" + cheval.getNom()+ " a lieu le " +this.getJour(date);
+		return "Le cours avec le cheval" + cheval.getNom()+ " a lieu le " +this.getJour(dateCours);
 	}
 	
 }
